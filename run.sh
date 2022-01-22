1b01acd7ad64dbaf44aeff6a9400738d9a123508
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-21 20:58:06 -0800

    Rename dataset

--- run.sh
+++ run.sh
@@ -4,15 +4,15 @@ set -e
 cd dbgen/
 make MACHINE=LINUX
 
-rm -rf /mnt/cephfs/dataset/*
-mkdir -p /mnt/cephfs/dataset/lineitem
-mkdir -p /mnt/cephfs/dataset/customer
-mkdir -p /mnt/cephfs/dataset/orders
-mkdir -p /mnt/cephfs/dataset/part
-mkdir -p /mnt/cephfs/dataset/partsupp
-mkdir -p /mnt/cephfs/dataset/supplier
-mkdir -p /mnt/cephfs/dataset/nation
-mkdir -p /mnt/cephfs/dataset/region
+rm -rf /mnt/cephfs/tpch_sf100/*
+mkdir -p /mnt/cephfs/tpch_sf100/lineitem
+mkdir -p /mnt/cephfs/tpch_sf100/customer
+mkdir -p /mnt/cephfs/tpch_sf100/orders
+mkdir -p /mnt/cephfs/tpch_sf100/part
+mkdir -p /mnt/cephfs/tpch_sf100/partsupp
+mkdir -p /mnt/cephfs/tpch_sf100/supplier
+mkdir -p /mnt/cephfs/tpch_sf100/nation
+mkdir -p /mnt/cephfs/tpch_sf100/region
 
 echo "Now execute : "
 echo "python3 gen.py <table> <path> <procs>"
