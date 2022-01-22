04903a47b110345338efbf4752d5f1cff50ef9c1
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-21 21:19:45 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@ set -e
 cd dbgen/
 make MACHINE=LINUX
 
-rm -rf /mnt/cephfs/tpch_sf100/*
 mkdir -p /mnt/cephfs/tpch_sf100/lineitem
 mkdir -p /mnt/cephfs/tpch_sf100/customer
 mkdir -p /mnt/cephfs/tpch_sf100/orders
@@ -14,8 +13,11 @@ mkdir -p /mnt/cephfs/tpch_sf100/supplier
 mkdir -p /mnt/cephfs/tpch_sf100/nation
 mkdir -p /mnt/cephfs/tpch_sf100/region
 
-echo "Now execute : "
-echo "python3 gen.py <table> <path>"
-
-# supplier: 140MB
-
+echo "python3 gen.py L /mnt/cephfs/tpch_sf100/lineitem 64" 
+echo "python3 gen.py O /mnt/cephfs/tpch_sf100/orders 64" 
+echo "python3 gen.py S /mnt/cephfs/tpch_sf100/partsupp 64" 
+echo "python3 gen.py P /mnt/cephfs/tpch_sf100/part 64" 
+echo "python3 gen.py c /mnt/cephfs/tpch_sf100/customer 64" 
+echo "python3 gen.py s /mnt/cephfs/tpch_sf100/supplier 64" 
+echo "python3 gen.py n /mnt/cephfs/tpch_sf100/nation 64" 
+echo "python3 gen.py r /mnt/cephfs/tpch_sf100/region 64" 
