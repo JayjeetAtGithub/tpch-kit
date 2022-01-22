791477552fdcaa6ca3524a5683894d840e590142
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-21 21:24:25 -0800

    Update gen.py

--- gen.py
+++ gen.py
@@ -4,9 +4,11 @@ import sys
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-def generate(index, num_procs, table):
-    os.system(f"dbgen -vf -s 100 -C {num_procs} -S {index} -T {table} -f")
-
+def generate(index, num_procs, table_shortcut):
+    if num_procs == 1:
+        os.system(f"dbgen -vf -s 100 -T {table_shortcut} -f")
+    else:
+        os.system(f"dbgen -vf -s 100 -C {num_procs} -S {index} -T {table_shortcut} -f")
 
 if __name__ == "__main__":
     os.chdir("dbgen")
@@ -15,10 +17,13 @@ if __name__ == "__main__":
     num_procs = int(sys.argv[3])
 
     os.environ["DSS_PATH"] = dataset_path
-    with ThreadPoolExecutor(max_workers=num_procs) as executor:
-        futures = list()
-        for index in range(num_procs):
-            futures.append(executor.submit(generate, index + 1, num_procs, table_shortcut))
+    if num_procs == 1:
+        generate(1, num_procs, table_shortcut)
+    else:
+        with ThreadPoolExecutor(max_workers=num_procs) as executor:
+            futures = list()
+            for index in range(num_procs):
+                futures.append(executor.submit(generate, index + 1, num_procs, table_shortcut))
 
-        for future in as_completed(futures):
-            print(future.result())
+            for future in as_completed(futures):
+                print(future.result())
