a75e09dfc84d4cbeb525d19cbe200597fcd8ebd0
Jayjeet Chakraborty <jc.github@rediffmail.com>
2022-01-21 22:36:24 -0800

    Fixes run.sh

--- run.sh
+++ run.sh
@@ -13,11 +13,11 @@ mkdir -p /mnt/cephfs/tpch_sf100/supplier
 mkdir -p /mnt/cephfs/tpch_sf100/nation
 mkdir -p /mnt/cephfs/tpch_sf100/region
 
-echo "python3 gen.py L /mnt/cephfs/tpch_sf100/lineitem 128" 
-echo "python3 gen.py O /mnt/cephfs/tpch_sf100/orders 64" 
-echo "python3 gen.py S /mnt/cephfs/tpch_sf100/partsupp 64" 
-echo "python3 gen.py P /mnt/cephfs/tpch_sf100/part 32" 
-echo "python3 gen.py c /mnt/cephfs/tpch_sf100/customer 32" 
-echo "python3 gen.py s /mnt/cephfs/tpch_sf100/supplier 10"
-echo "python3 gen.py n /mnt/cephfs/tpch_sf100/nation 1" 
-echo "python3 gen.py r /mnt/cephfs/tpch_sf100/region 1" 
+python3 gen.py L /mnt/cephfs/tpch_sf100/lineitem 128
+python3 gen.py O /mnt/cephfs/tpch_sf100/orders 64
+python3 gen.py S /mnt/cephfs/tpch_sf100/partsupp 64 
+python3 gen.py P /mnt/cephfs/tpch_sf100/part 32 
+python3 gen.py c /mnt/cephfs/tpch_sf100/customer 32 
+python3 gen.py s /mnt/cephfs/tpch_sf100/supplier 10
+python3 gen.py n /mnt/cephfs/tpch_sf100/nation 1 
+python3 gen.py r /mnt/cephfs/tpch_sf100/region 1 
