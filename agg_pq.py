b05775dddb5a2f8434b329ab3c2f89d51f5578f6
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-09 12:44:58 -0700

    Use df.concat

--- agg_pq.py
+++ agg_pq.py
@@ -23,8 +23,9 @@ if __name__ == "__main__":
     for table in tables:
         table_parquet_dir = os.path.join(base_dir, table, "parquet")
         table_parquet_files = os.listdir(table_parquet_dir)
-        base_df = pd.read_parquet(os.path.join(table_parquet_dir, table_parquet_files[0]))
-        for file in table_parquet_files[1:]:
+        df_list = list()
+        for file in table_parquet_files:
             df = pd.read_parquet(os.path.join(table_parquet_dir, file))
-            base_df = base_df.append(df)
-        base_df.to_parquet(os.path.join(base_dir, table + ".parquet"))
+            df_list.append(df)
+        final_df = pd.concat(df_list)
+        final_df.to_parquet(os.path.join(base_dir, table + ".parquet"))
