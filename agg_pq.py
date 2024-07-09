f999e2fc176a934cfecd5850a63b1d1e71fd87a1
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-09 12:31:50 -0700

    Add script to agg the parquet files

--- agg_pq.py
+++ agg_pq.py
@@ -2,9 +2,24 @@ import pandas as pd
 
 
 if __name__ == "__main__":
-    parquet_file_list = str(sys.argv[1])
-    base_df = pd.read_parquet(parquet_file_list[0])
-    for file in parquet_file_list[1:]:
-        df = pd.read_parquet(file)
-        base_df = base_df.append(df)
-    base_df.to_parquet("agg.parquet")
+    base_dir = str(sys.argv[1])
+
+    tables = [
+        "customer",
+        "lineitem",
+        "nation",
+        "orders",
+        "part",
+        "partsupp",
+        "supplier",
+        "region"
+    ]
+
+    for table in tables:
+        table_parquet_dir = os.path.join(base_dir, table, "parquet")
+        table_parquet_files = os.listdir(table_parquet_dir)
+        base_df = pd.read_parquet(os.path.join(table_parquet_dir, table_parquet_files[0]))
+        for file in table_parquet_files[1:]:
+            df = pd.read_parquet(os.path.join(table_parquet_dir, file))
+            base_df = base_df.append(df)
+        base_df.to_parquet(os.path.join(base_dir, table + ".parquet"))
