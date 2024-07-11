9699b11efdcaa661659c51c07a51cd8281786439
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-11 10:14:26 -0700

    Add script to cast cols

--- cast.py
+++ cast.py
@@ -17,21 +17,21 @@ if __name__ == "__main__":
             df["l_shipdate"] = pd.to_datetime(df["l_shipdate"]).dt.strftime("%Y-%m-%d")
             df["l_commitdate"] = pd.to_datetime(df["l_commitdate"]).dt.strftime("%Y-%m-%d")
             df["l_receiptdate"] = pd.to_datetime(df["l_receiptdate"]).dt.strftime("%Y-%m-%d")
-            df.to_parquet(filepath, index=False)
+            df.to_parquet(filepath, index=False, engine="pyarrow")
         
         elif filepath.endswith("part.parquet"):
             df = pd.read_parquet(filepath)
             df["p_size"] = df["p_size"].astype("int64")
-            df.to_parquet(filepath, index=False)
+            df.to_parquet(filepath, index=False, engine="pyarrow")
 
         elif filepath.endswith("partsupp.parquet"):
             df = pd.read_parquet(filepath)
             df["ps_availqty"] = df["ps_availqty"].astype("int64")
-            df.to_parquet(filepath, index=False)
+            df.to_parquet(filepath, index=False, engine="pyarrow")
 
         elif filepath.endswith("orders.parquet"):
             df = pd.read_parquet(filepath)
             df["o_shippriority"] = df["o_shippriority"].astype("int64")
             df["o_orderdate"] = pd.to_datetime(df["o_orderdate"]).dt.strftime("%Y-%m-%d")
-            df.to_parquet(filepath, index=False)
+            df.to_parquet(filepath, index=False, engine="pyarrow")
         
