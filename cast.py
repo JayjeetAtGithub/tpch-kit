ee03aec7a6781e322c50c31c75267fbdcc33e868
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-11 10:34:28 -0700

    Add script to cast cols

--- cast.py
+++ cast.py
@@ -17,21 +17,21 @@ if __name__ == "__main__":
             df["l_shipdate"] = pd.to_datetime(df["l_shipdate"])
             df["l_commitdate"] = pd.to_datetime(df["l_commitdate"])
             df["l_receiptdate"] = pd.to_datetime(df["l_receiptdate"])
-            df.to_parquet(filepath, index=False, engine="pyarrow")
+            df.to_parquet(filepath, index=False)
         
         elif filepath.endswith("part.parquet"):
             df = pd.read_parquet(filepath)
             df["p_size"] = df["p_size"].astype("int64")
-            df.to_parquet(filepath, index=False, engine="pyarrow")
+            df.to_parquet(filepath, index=False)
 
         elif filepath.endswith("partsupp.parquet"):
             df = pd.read_parquet(filepath)
             df["ps_availqty"] = df["ps_availqty"].astype("int64")
-            df.to_parquet(filepath, index=False, engine="pyarrow")
+            df.to_parquet(filepath, index=False)
 
         elif filepath.endswith("orders.parquet"):
             df = pd.read_parquet(filepath)
             df["o_shippriority"] = df["o_shippriority"].astype("int64")
             df["o_orderdate"] = pd.to_datetime(df["o_orderdate"])
-            df.to_parquet(filepath, index=False")
+            df.to_parquet(filepath, index=False)
         
