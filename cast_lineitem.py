5a08336e4481349433f106cded0f4f0ebcf0410c
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-11 09:17:33 -0700

    Add script to cast cols

---
+++ cast_lineitem.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas as pd
+
+if __name__ == "__main__":
+    filepath = str(sys.argv[1])
+    df = pd.read_parquet(filepath)
+
+    df["l_quantity"] = df["l_quantity"].astype("int64")
+
+    df.to_parquet(filepath)
