bcc5c9b8515c9e1e64acdfbe26015f88ace983a0
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-11 09:28:34 -0700

    Add script to cast cols

--- cast_lineitem.py
+++ cast_lineitem.py
@@ -4,9 +4,25 @@ import pandas as pd
 
 if __name__ == "__main__":
     filepath = str(sys.argv[1])
-    df = pd.read_parquet(filepath)
 
-    df["l_linenumber"] = df["l_linenumber"].astype("int64")
-    df["l_quantity"] = df["l_quantity"].astype("int64")
+    if filepath.endswith("lineitem.parquet"):
+        df = pd.read_parquet(filepath)
+        df["l_linenumber"] = df["l_linenumber"].astype("int64")
+        df["l_quantity"] = df["l_quantity"].astype("int64")
+        df.to_parquet(filepath, index=False)
+    
+    elif filepath.endswith("part.parquet"):
+        df = pd.read_parquet(filepath)
+        df["p_size"] = df["p_size"].astype("int64")
+        df.to_parquet(filepath, index=False)
 
-    df.to_parquet(filepath, index=False)
+    elif filepath.endswith("partsupp.parquet"):
+        df = pd.read_parquet(filepath)
+        df["ps_availqty"] = df["ps_availqty"].astype("int64")
+        df.to_parquet(filepath, index=False)
+
+    elif filepath.endswith("orders.parquet"):
+        df = pd.read_parquet(filepath)
+        df["o_shippriority"] = df["o_shippriority"].astype("int64")
+        df.to_parquet(filepath, index=False)
+    
