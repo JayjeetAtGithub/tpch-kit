13a59e18b5129552ee59a2289f010cf17f02be86
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-11 09:22:46 -0700

    Add script to cast cols

--- cast_lineitem.py
+++ cast_lineitem.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     filepath = str(sys.argv[1])
     df = pd.read_parquet(filepath)
 
+    df["l_linenumber"] = df["l_linenumber"].astype("int64")
     df["l_quantity"] = df["l_quantity"].astype("int64")
 
     df.to_parquet(filepath, index=False)
