7e3e97ddf882e4e1089aede7e0b2900a3221ae8d
JayjeetAtGithub <jc.github@rediffmail.com>
2024-07-16 13:47:27 -0700

    refactoring

--- gen_raw_tables.sh
+++ gen_raw_tables.sh
@@ -13,11 +13,11 @@ mkdir -p ${dataset_dir}/supplier
 mkdir -p ${dataset_dir}/nation
 mkdir -p ${dataset_dir}/region
 
-python3 gen.py L ${dataset_dir}/lineitem 96 ${scale_factor}
-python3 gen.py O ${dataset_dir}/orders 96 ${scale_factor}
-python3 gen.py S ${dataset_dir}/partsupp 64 ${scale_factor}
-python3 gen.py P ${dataset_dir}/part 32 ${scale_factor}
-python3 gen.py c ${dataset_dir}/customer 32 ${scale_factor}
-python3 gen.py s ${dataset_dir}/supplier 10 ${scale_factor}
-python3 gen.py n ${dataset_dir}/nation 1 ${scale_factor}
-python3 gen.py r ${dataset_dir}/region 1 ${scale_factor}
+python3 gen_raw.py L ${dataset_dir}/lineitem 96 ${scale_factor}
+python3 gen_raw.py O ${dataset_dir}/orders 96 ${scale_factor}
+python3 gen_raw.py S ${dataset_dir}/partsupp 64 ${scale_factor}
+python3 gen_raw.py P ${dataset_dir}/part 32 ${scale_factor}
+python3 gen_raw.py c ${dataset_dir}/customer 32 ${scale_factor}
+python3 gen_raw.py s ${dataset_dir}/supplier 10 ${scale_factor}
+python3 gen_raw.py n ${dataset_dir}/nation 1 ${scale_factor}
+python3 gen_raw.py r ${dataset_dir}/region 1 ${scale_factor}
