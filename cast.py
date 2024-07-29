ffee51ecf22f2dc4183f38f9838efdad084cb01b
JayjeetAtGithub <JayjeetAtGithub@users.noreply.github.com>
2024-07-29 11:30:12 -0700

    Cast to float64

--- cast.py
+++ cast.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 if __name__ == "__main__":
     dataset_path = str(sys.argv[1])
-    tables = ["lineitem", "orders", "part", "partsupp"]
+    tables = ["lineitem", "part", "partsupp", "orders", "supplier", "customer"]
     for table in tables:
         filepath = os.path.join(dataset_path, f"{table}.parquet")
         print("Reading file ", filepath)
@@ -13,24 +13,35 @@ if __name__ == "__main__":
         if filepath.endswith("lineitem.parquet"):
             df = pd.read_parquet(filepath)
             df["l_linenumber"] = df["l_linenumber"].astype("int64")
-            df["l_quantity"] = df["l_quantity"].astype("int64")
-            df["l_shipdate"] = df["l_extendedprice"].astype("str")
-            df["l_commitdate"] = df["l_commitdate"].astype("str")
-            df["l_receiptdate"] = df["l_receiptdate"].astype("str")
+            df["l_extendedprice"] = df["l_extendedprice"].astype("float64")
+            df["l_discount"] = df["l_discount"].astype("float64")
+            df["l_tax"] = df["l_tax"].astype("float64")
             df.to_parquet(filepath, index=False)
         
         elif filepath.endswith("part.parquet"):
             df = pd.read_parquet(filepath)
             df["p_size"] = df["p_size"].astype("int64")
+            df["p_retailprice"] = df["p_retailprice"].astype("float64")
             df.to_parquet(filepath, index=False)
 
         elif filepath.endswith("partsupp.parquet"):
             df = pd.read_parquet(filepath)
             df["ps_availqty"] = df["ps_availqty"].astype("int64")
+            df["ps_supplycost"] = df["ps_supplycost"].astype("float64")
             df.to_parquet(filepath, index=False)
 
         elif filepath.endswith("orders.parquet"):
             df = pd.read_parquet(filepath)
+            df["o_totalprice"] = df["o_totalprice"].astype("float64")
             df["o_shippriority"] = df["o_shippriority"].astype("int64")
-            df["o_orderdate"] = df["o_orderdate"].astype("str")
+            df.to_parquet(filepath, index=False)
+
+        elif filepath.endswith("supplier.parquet"):
+            df = pd.read_parquet(filepath)
+            df["s_acctbal"] = df["s_acctbal"].astype("float64")
+            df.to_parquet(filepath, index=False)
+
+        elif filepath.endswith("customer.parquet"):
+            df = pd.read_parquet(filepath)
+            df["c_acctbal"] = df["c_acctbal"].astype("float64")
             df.to_parquet(filepath, index=False)
